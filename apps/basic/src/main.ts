31a976a452b0b2a3088add1e598e325314e7d46b
Himank Barve <hbarve1592@gmail.com>
2023-02-09 18:33:12 +0530

    Basic graph complete

--- apps/basic/src/main.ts
+++ apps/basic/src/main.ts
@@ -1,1 +1,25 @@
+import ForceGraph3D from '3d-force-graph';
 
+const div = document.createElement('div');
+div.id = '3d-graph';
+document.body.appendChild(div);
+
+const N = 1000;
+// console.log(Array(N));
+// console.log(Array(N).keys());
+const list = Array.from({ length: N }, (_, i) => ({ id: i }));
+
+const gData = {
+  // nodes: [...Array(N).keys()].map((i) => ({ id: i })),
+  nodes: list,
+  // links: [],
+  links: list
+    .slice(1)
+    // .filter((id: number) => id % 2 === 0)
+    .map(({ id }) => ({
+      source: id,
+      target: Math.round(Math.random() * (id - 1)),
+    })),
+};
+
+const Graph = ForceGraph3D()(div).graphData(gData);
